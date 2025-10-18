863e42db327b0fbd4ded6ccd4b8e5a93e627fb06
PhilippePerret <philippe.perret@yahoo.fr>
2025-10-18 16:35:10 +0200

    minor change

--- spec/app/builder_spec.rb
+++ spec/app/builder_spec.rb
@@ -1,17 +1,19 @@
 describe "Builder" do
+
+  Builder = PerfectMail::Builder
   
   it "produit les fichiers attendus" do
     path = File.absolute_path(File.join('.', 'spec', 'fixtures', 'mails', 'simple.pmail'))
-    PerfectMail::Builder.new(path).build({})
+    Builder.new(path).build({})
   end
 
-  # it "produit le fichier pour Mail.app" do
-  #   path = File.absolute_path(File.join('.', 'spec', 'fixtures', 'mails', 'simple.pmail'))
-  #   PerfectMail::Builder.new(path).build({mail_app: true})
-  # end
+  it "produit le fichier pour Mail.app" do
+    path = File.absolute_path(File.join('.', 'spec', 'fixtures', 'mails', 'simple.pmail'))
+    Builder.new(path).build({mail_app: true})
+  end
 
   it "produces a mail for Mail.app with all the features (and opens it)" do
     fpath = fixture_path('mails/full.pmail')
-    PerfectMail::Builder.new(fpath).build({mail_app: true})
+    Builder.new(fpath).build({mail_app: true})
   end
 end
